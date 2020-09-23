38a2571542c2e8f5b403c45739dff55075e381be
Yegor Balamutin <yegorbalamutin1@gmail.com>
2020-09-23 21:38:07 +0300

    first commit

---
+++ Lab/03/C++/задание 3/задание 3/задание 3.cpp
@@ -0,0 +1,20 @@
+﻿
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    double a;
+    int b;
+    cout << "a= ";
+    cin >> a;
+    cout << "b= ";
+    cin >> b;
+    cout << "a + b = " << a + b << endl;
+    cout << "a - b = " << a - b << endl;
+    cout << "a * b = " << a * b << endl;
+    cout << "a / b = " << a / b << endl;
+}
+
