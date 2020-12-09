c45f31bb029105133019affa96fde49f81d6bef8
Yegor Balamutin <yegorbalamutin1@gmail.com>
2020-12-09 15:01:56 +0300

    begin to create a template for 26

---
+++ Practice/26/C++/task26/task26.cpp
@@ -0,0 +1,19 @@
+﻿#include <iostream>
+#include <vector>
+
+template<typename T>
+T BozoSort(T arr, bool asc = true) {
+	return arr;
+}
+
+int main()
+{
+	int n = 4;
+	std::vector<int> arr = { 1, 2, 3, 4 };
+	std::vector<int> res;
+	res = BozoSort<std::vector<int>>(arr);
+	for (int i = 0; i < n; i++) {
+		std::cout << res[i] << " ";
+	}
+	return 0;
+}
