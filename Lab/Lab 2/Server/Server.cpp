170c99a6655faa47590765737a153ecce172b316
Yegor Balamutin <yegorbalamutin1@gmail.com>
2020-12-22 15:17:20 +0300

    task 29 completed

--- Lab/Lab 2/Server/Server.cpp
+++ Lab/Lab 2/Server/Server.cpp
@@ -7,53 +7,105 @@
 using namespace httplib;
 using json = nlohmann::json;
 
-std::ofstream logger("log.txt");
+void gen_res_webhooks(const Request& req, Response& res);
 
 // В этой функции формируем ответ сервера на запрос
-void gen_response(const Request& req, Response& res) {
-    logger << u8"starting function\n";
+void gen_res_edit_webhooks(const Request& req, Response& res) {
+    std::cout << "Recived a Post-request on \"\\webhooks\"\n";
+    if (req.has_param("del")) {
+        std::cout << "request for deliting \"" << req.get_param_value("del") << "\"\n";
+    }
+    else {
+        std::cout << "request for adding \"" << req.get_param_value("set") << "\"\n";
+    }
     json conf;
     std::ifstream file1("configuration.json");
     if (file1.is_open()) {
-        logger << u8"conf file is opened\n";
+        std::cout << "conf file is opened\n";
         file1 >> conf;
         file1.close();
     }
     else {
-        logger << u8"conf file is not founded\n";
+        std::cout << "conf file is not founded\n";
         conf["webhooks"] = json::array();
         std::ofstream file1("configuration.json");
         file1 << std::setw(2) << conf;
         file1.close();
     }
     // Выводим на экран тело запроса
-    std::cout << req.body.c_str();
+    std::cout << req.body.c_str() << std::endl;
     // Здесь будет ответ, пока-что взят пример из документации
-    std::string str = u8R"(
-    {
-      "response": {
-        "text": "Здравствуйте! Это мы, хороводоведы.",
-        "tts": "Здравствуйте! Это мы, хоров+одо в+еды.",
-        "buttons": [
-            {
-                "title": "Надпись на кнопке",
-                "payload": {},
-                "url": "https://example.com/",
-                "hide": true
-            }
-        ],
-        "end_session": false
-      },
-      "version": "1.0"
-    })";
-    logger.close();
+    std::string str = "";
     // Отправляем ответ
     res.set_content(str, "text/json; charset=UTF-8");
+    std::cout << "function gen_res_edit_webhooks finished" << std::endl << std::endl;
 }
 
 int main() {
-    Server svr;                            // Создаём сервер (пока-что не запущен)
-    svr.Post("/", gen_response);           // Вызвать функцию gen_response на post запрос
-    logger << u8"Start server... OK\n";    // cout использовать нельзя
-    svr.listen("localhost", 1234);         // Запускаем сервер на localhost и порту 1234
+    Server svr;                                      // Создаём сервер (пока-что не запущен)
+    svr.Post("/webhooks", gen_res_edit_webhooks);    // Вызвать функцию gen_response на post запрос
+    svr.Get("/webhooks", gen_res_webhooks);
+    std::cout << "Starting server... OK\n";
+    svr.listen("localhost", 1234);                   // Запускаем сервер на localhost и порту 1234
+}
+
+// В этой функции заполняется и отправляется html шаблон с вебхуками
+void gen_res_webhooks(const Request& req, Response& res) {
+    std::cout << "Recived a Get-request on \"\\webhooks\"\n";
+    // загрузка файла конфигурации
+    json conf;
+    std::ifstream file1("configuration.json");
+    if (file1.is_open()) {
+        std::cout << "conf file is opened\n";
+        file1 >> conf;
+        file1.close();
+    }
+    else {
+        std::cout << "conf file is not founded\n";
+        conf["webhooks"] = json::array();
+        std::ofstream file1("configuration.json");
+        file1 << std::setw(2) << conf;
+        file1.close();
+    }
+    // чтение html шаблона
+    std::ifstream file2("Dashboard.html");
+    std::string html_temp;
+    if (file2.is_open()) {
+        std::getline(file2, html_temp, '\0');
+    }
+    else {
+        std::cout << "failed open html file\n";
+        std::cout << "function gen_response_webhooks finished\n";
+        return;
+    }
+    // заполнение html шаблона
+    std::string plug = u8"{webhooks_list}";
+    if (size(conf[u8"webhooks"]) == 0) {
+        std::cout << "no any webhooks\n";
+    }
+    else {
+        std::cout << "webhooks exist:\n";
+        std::string webhook = u8R"(
+     <div class="form-row align-items-center">
+        <div class="col">
+        <input type="text" value="{Webhook URL}" class="form-control mb-2" disabled>
+        </div>
+ 
+        <div class="col">
+            <button type="submit" name="del" value="{Webhook URL}" class="btn btn-danger mb-2">Удалить</button>
+        </div>
+     </div>)";
+        webhook += plug;
+        std::string plug1 = u8"{Webhook URL}";
+        for (std::string i : conf["webhooks"]) {
+            std::cout << i << std::endl;
+            html_temp = html_temp.replace(html_temp.find(plug), size(plug), webhook);
+            html_temp = html_temp.replace(html_temp.find(plug1), size(plug1), i);
+        }
+    }
+    html_temp = html_temp.replace(html_temp.find(plug), size(plug), "");
+    // отправка заполненного html шаблона
+    res.set_content(html_temp, "text/html");
+    std::cout << "response sent\n";
+    std::cout << "function gen_response_webhooks finished" << std::endl << std::endl;
 }
---
+++ Practice/29/C++/task29/task29.cpp
@@ -0,0 +1,138 @@
+﻿#include <iostream>
+#include <string>
+#include <vector>
+#include <map>
+#include <ctime>
+
+struct Student {
+	std::string name;
+	int group;
+	std::map<std::string, int> exams;
+};
+
+std::string adding_spc(std::string name) {
+	while (size(name) < 11) {
+		name += " ";
+	}
+	return name;
+}
+
+std::ostream& operator<< (std::ostream& out, Student stud) {
+	out << "+------------+-------+------+------+------+------+\n";
+	out << "| Name       | Group | Math | Phys | Hist | Prog |\n";
+	out << "+------------+-------+------+------+------+------+\n";
+	out << "| " << adding_spc(stud.name)
+		<< "| " << stud.group << "     "
+		<< "| " << stud.exams["mathematics"] << "    "
+		<< "| " << stud.exams["physics"] << "    "
+		<< "| " << stud.exams["history"] << "    "
+		<< "| " << stud.exams["programming"] << "    |\n"
+		<< "+------------+-------+------+------+------+------+\n";
+	return out;
+}
+
+std::ostream& operator<< (std::ostream& out, std::vector<Student> vec) {
+	std::cout << "+------------+-------+------+------+------+------+\n";
+	std::cout << "| Name       | Group | Math | Phys | Hist | Prog |\n";
+	std::cout << "+------------+-------+------+------+------+------+\n";
+	for (auto a : vec) {
+		std::cout << "| " << adding_spc(a.name)
+		<< "| " << a.group << "     "
+		<< "| " << a.exams["mathematics"] << "    "
+		<< "| " << a.exams["physics"] << "    "
+		<< "| " << a.exams["history"] << "    "
+		<< "| " << a.exams["programming"] << "    |\n"
+		<< "+------------+-------+------+------+------+------+\n";
+	}
+	return out;
+}
+
+bool operator> (Student a, Student b) {
+	if (a.name > b.name) {
+		return true;
+	}
+	else {
+		return false;
+	}
+}
+
+bool operator< (Student a, Student b) {
+	if (a.name < b.name) {
+		return true;
+	}
+	else {
+		return false;
+	}
+}
+
+template <typename T>
+std::vector<T> BozoSort(std::vector<T> arr, bool ascend = true) {
+	int a, b;
+	bool sorted = false;
+	while (!sorted) {
+		if (ascend) {
+			sorted = true;
+			for (int i = 0; i < size(arr) - 1; i++) {
+				if (arr[i] > arr[i + 1]) {
+					sorted = false;
+					break;
+				}
+			}
+		}
+		else {
+			sorted = true;
+			for (int i = 0; i < size(arr) - 1; i++) {
+				if (arr[i] < arr[i + 1]) {
+					sorted = false;
+					break;
+				}
+			}
+		}
+		if (!sorted) {
+			a = rand() % size(arr);
+			b = rand() % size(arr);
+			std::swap(arr[a], arr[b]);
+		}
+	}
+	return arr;
+}
+
+int main()
+{
+	// Блок рандомного заполнения вектора Students
+	srand(time(0));
+	std::vector<std::string> human_names = { "Alex", "Devid", "Bob", "Wilson", "Steve", "Ben", "Robert", "Alice", "Mary", "Windy", "Katy", "Emma" };
+	std::string letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+	std::vector<int> marks = { 2, 3, 3, 3, 4, 4, 4, 4, 5, 5 };
+	std::vector<Student> Students;
+	Student new_stud;
+	for (int i = 0; i < 10; i++) {
+		new_stud.name = human_names[rand() % size(human_names)] + " " + letters[rand() % size(letters)];
+		new_stud.group = (rand() % 9) + 1;
+		new_stud.exams["mathematics"] = marks[rand() % size(marks)];
+		new_stud.exams["physics"] = marks[rand() % size(marks)];
+		new_stud.exams["history"] = marks[rand() % size(marks)];
+		new_stud.exams["programming"] = marks[rand() % size(marks)];
+		Students.push_back(new_stud);
+	}
+
+	std::vector<Student> bad;
+	for (Student stud : Students) {
+		for (auto mark : stud.exams) {
+			if (mark.second == 2) {
+				bad.push_back(stud);
+				break;
+			}
+		}
+	}
+	if (size(bad) == 0) {
+		std::cout << "Not found";
+	}
+	else {
+		bad = BozoSort(bad);
+		std::cout << bad << std::endl;
+		std::cout << "Expulsion" << std::endl;
+		std::cout << bad[rand() % size(bad)];
+	}
+	return 0;
+}
