e4b0f8181e1f30c6b99402413ed21b80e169fedb
Yegor Balamutin <yegorbalamutin1@gmail.com>
2020-12-26 20:55:44 +0300

    webhooks ready

--- Lab/Lab 2/Server/Server.cpp
+++ Lab/Lab 2/Server/Server.cpp
@@ -8,38 +8,8 @@ using namespace httplib;
 using json = nlohmann::json;
 
 void gen_res_webhooks(const Request& req, Response& res);
-
-// В этой функции формируем ответ сервера на запрос
-void gen_res_edit_webhooks(const Request& req, Response& res) {
-    std::cout << "Recived a Post-request on \"\\webhooks\"\n";
-    if (req.has_param("del")) {
-        std::cout << "request for deliting \"" << req.get_param_value("del") << "\"\n";
-    }
-    else {
-        std::cout << "request for adding \"" << req.get_param_value("set") << "\"\n";
-    }
-    json conf;
-    std::ifstream file1("configuration.json");
-    if (file1.is_open()) {
-        std::cout << "conf file is opened\n";
-        file1 >> conf;
-        file1.close();
-    }
-    else {
-        std::cout << "conf file is not founded\n";
-        conf["webhooks"] = json::array();
-        std::ofstream file1("configuration.json");
-        file1 << std::setw(2) << conf;
-        file1.close();
-    }
-    // Выводим на экран тело запроса
-    std::cout << req.body.c_str() << std::endl;
-    // Здесь будет ответ, пока-что взят пример из документации
-    std::string str = "";
-    // Отправляем ответ
-    res.set_content(str, "text/json; charset=UTF-8");
-    std::cout << "function gen_res_edit_webhooks finished" << std::endl << std::endl;
-}
+void gen_res_edit_webhooks(const Request& req, Response& res);
+std::string gen_html();
 
 int main() {
     Server svr;                                      // Создаём сервер (пока-что не запущен)
@@ -52,6 +22,59 @@ int main() {
 // В этой функции заполняется и отправляется html шаблон с вебхуками
 void gen_res_webhooks(const Request& req, Response& res) {
     std::cout << "Recived a Get-request on \"\\webhooks\"\n";
+    std::string html_temp = gen_html();
+    // отправка заполненного html шаблона
+    res.set_content(html_temp, "text/html");
+    std::cout << "response sent\n";
+    std::cout << "function gen_response_webhooks finished" << std::endl << std::endl;
+}
+
+// В этой функции добавляется или удаляется вебхук и отправляется ответ
+void gen_res_edit_webhooks(const Request& req, Response& res) {
+    std::cout << "Recived a Post-request on \"\\webhooks\"\n";
+    // Открытие конфигурационного файла
+    json conf;
+    std::ifstream file1("configuration.json");
+    if (file1.is_open()) {
+        std::cout << "conf file is opened\n";
+        file1 >> conf;
+        file1.close();
+    }
+    else {
+        std::cout << "conf file is not founded\n";
+        return;
+    }
+    // Проверка на то, поступил ли запрос на добавление или на удаление
+    if (req.has_param("del")) {
+        std::cout << "request for deliting \"" << req.get_param_value("del") << "\"\n";
+        auto val = req.get_param_value("del");
+        // удаление из файла конфигурации
+        for (int i = 0; i < size(conf["webhooks"]); i++) {
+            if (conf["webhooks"][i] == val) {
+                conf["webhooks"].erase(conf["webhooks"].begin() + i);
+                break;
+            }
+        }
+    }
+    else {
+        std::cout << "request for adding \"" << req.get_param_value("set") << "\"\n";
+        // добавление в файл конфигурации
+        auto val = req.get_param_value("set");
+        conf["webhooks"].push_back(val);
+    }
+    // Запись изменений в файл
+    std::ofstream file2("configuration.json");
+    file2 << std::setw(2) << conf;
+    file2.close();
+    // Формируем и отправляем ответ
+    std::string str = gen_html();
+    res.set_content(str, "text/html; charset=UTF-8");
+    std::cout << "function gen_res_edit_webhooks finished" << std::endl << std::endl;
+}
+
+// Функция заполняет html шаблон по файлу конфигурации и возвращает его в виде строки
+std::string gen_html() {
+    std::cout << "Starting function gen_html\n";
     // загрузка файла конфигурации
     json conf;
     std::ifstream file1("configuration.json");
@@ -68,7 +91,7 @@ void gen_res_webhooks(const Request& req, Response& res) {
         file1.close();
     }
     // чтение html шаблона
-    std::ifstream file2("Dashboard.html");
+    std::ifstream file2("dashboard.html");
     std::string html_temp;
     if (file2.is_open()) {
         std::getline(file2, html_temp, '\0');
@@ -76,7 +99,7 @@ void gen_res_webhooks(const Request& req, Response& res) {
     else {
         std::cout << "failed open html file\n";
         std::cout << "function gen_response_webhooks finished\n";
-        return;
+        return "";
     }
     // заполнение html шаблона
     std::string plug = u8"{webhooks_list}";
@@ -101,11 +124,9 @@ void gen_res_webhooks(const Request& req, Response& res) {
             std::cout << i << std::endl;
             html_temp = html_temp.replace(html_temp.find(plug), size(plug), webhook);
             html_temp = html_temp.replace(html_temp.find(plug1), size(plug1), i);
+            html_temp = html_temp.replace(html_temp.find(plug1), size(plug1), i);
         }
     }
     html_temp = html_temp.replace(html_temp.find(plug), size(plug), "");
-    // отправка заполненного html шаблона
-    res.set_content(html_temp, "text/html");
-    std::cout << "response sent\n";
-    std::cout << "function gen_response_webhooks finished" << std::endl << std::endl;
+    return html_temp;
 }
